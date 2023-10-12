5a95cac12bec18a1ba0f7fdec9174c3174703327
SummaDiaboli <salimabdu008@gmail.com>
2023-10-12 18:30:12 +0100

    updated pages

---
+++ src/components/NavLinks.tsx
@@ -0,0 +1,55 @@
+import React, { useState, type LinkHTMLAttributes, useEffect } from "react"
+
+const NavLinks = () => {
+    const navLinks = [
+        { name: "About", link: "#about" },
+        { name: "Experience", link: "#experience" },
+    ]
+
+    const [activeAnchor, setActiveAnchor] = useState("")
+
+    const changeActiveAnchor = (el: HTMLAnchorElement) => {
+        setActiveAnchor(el.hash)
+    }
+
+    const updateAnchor = () => {
+        setActiveAnchor(location.hash)
+    }
+
+    useEffect(() => {
+        setActiveAnchor(`#${document.URL.split("#")[1]}`)
+    }, [])
+
+    return (
+        <div onScroll={updateAnchor}>
+            <ul className="flex flex-col space-y-3">
+                {navLinks.map((nav, index) => (
+                    <a
+                        key={`nav${index}`}
+                        href={nav.link}
+                        className={`flex max-w-fit flex-row items-center gap-x-2 group ${
+                            nav.link === activeAnchor
+                                ? "text-slate-200"
+                                : "text-slate-500"
+                        }`}
+                        onClick={(e) => changeActiveAnchor(e.currentTarget)}
+                    >
+                        <div
+                            className={`group-hover:w-14 group-hover:bg-slate-200 transition-all ${
+                                nav.link === activeAnchor
+                                    ? "w-14 h-[0.08rem] bg-slate-200"
+                                    : "w-7 h-[0.08rem] bg-slate-500"
+                            }`}
+                        />
+
+                        <p className="uppercase text-xs leading-tight tracking-widest font-bold group-hover:text-slate-200">
+                            {nav.name}
+                        </p>
+                    </a>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default NavLinks
